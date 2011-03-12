1ed8676e1ef8dc54acda84f98109f36e52facc03
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2011-03-12 11:04:30 +0100

    [preview] Adding preview feature
        - Since I added a /preview/ rewriterule, here is the preview code.
        By going to /preview/, you'll see the LAST post (and only this one)
        with your template, rendered as HTML. This should allow you to be
        sure of your posts when publishing it "for real".
        Since it's PHP, you can change/generate by just refreshing the page
        (wow !)

---
+++ _inc/preview.php
@@ -0,0 +1,43 @@
+<?php
+# Author:     Maxime Hadjinlian
+#             maxime.hadjinlian@gmail.com
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+include(str_replace('//','/',dirname(__FILE__).'/') .'../config.php');
+require_once ROOT_DIR."/".INC_DIR."/functions.php";
+
+include_once ROOT_DIR.'/'.LAYOUT_DIR.'/header.php';
+
+$files = glob(ROOT_DIR.'/'.POST_DIR.'/*.md');
+$size = sizeOf($files);
+
+$emp = array();
+$info = extract_info($files[$size-1], $emp);
+echo post_to_html($info, True, false);
+
+include_once ROOT_DIR.'/'.LAYOUT_DIR.'/sidebar.php';
+include_once ROOT_DIR.'/'.LAYOUT_DIR.'/footer.php';
+
+?>
